80f3c50bcc595aaf951964349232837ba9ac9e71
AVINASH PANDEY <98386256+Avi2492@users.noreply.github.com>
2023-12-12 14:50:44 +0530

    Add files via upload

---
+++ Lec01/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/twitter", (req, res) => {
+  res.send("avinash");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>Please Login</h1>");
+});
+
+app.get("/chaiaurcode", (req, res) => {
+  res.send("<h2>Chai Aur Code</h2>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
